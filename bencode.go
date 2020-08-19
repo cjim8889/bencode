5c5c2472565214852839630a4cece1a1f6df4db6
Wuhao Chen <wuhaochen@192.168.0.106>
2020-08-19 23:45:53 +0800

    implement parse bytes

--- bencode.go
+++ bencode.go
@@ -12,11 +12,11 @@ type BencodeReader struct {
 	reader *bufio.Reader
 }
 
-type ParseIntError struct {
+type ParseError struct {
 	error string
 }
 
-func (e ParseIntError) Error() string {
+func (e ParseError) Error() string {
 	return fmt.Sprintf("Parse Int Error: %v\n", e.error)
 }
 
@@ -31,8 +31,8 @@ func (r *BencodeReader) DecodeStream() (interface{}, error)  {
 			break
 		}
 
-		switch b[0] {
-		case 'i': {
+		switch c := b[0]; {
+		case c == 'i': {
 			result, err := ParseInt(r)
 			if err != nil {
 				return nil, err
@@ -40,76 +40,94 @@ func (r *BencodeReader) DecodeStream() (interface{}, error)  {
 
 			return result, nil
 		}
+		case c >= 48 && c <= 57: {
+			result, err := ParseByteString(r)
+			if err != nil {
+				return nil, err
+			}
+
+			return result, nil
+		}
+			
 		}
 	}
 
 	return nil, nil
 }
 
+func ParseByteString(r *BencodeReader) ([]byte, error) {
+	byteCountA, err := r.reader.ReadBytes(':')
+	if err != nil {
+		return nil, ParseError{}
+	}
+
+	strRep := string(byteCountA[:len(byteCountA) - 1])
+	byteCount, err := strconv.Atoi(strRep)
+	if err != nil || byteCount <= 0 {
+		return nil, ParseError{}
+	}
+
+	result := make([]byte, byteCount)
+	n, err := r.reader.Read(result)
+	if err != nil {
+		return nil, ParseError{}
+	}
+
+	if byteCount != n {
+		return nil, ParseError{}
+	}
+
+	return result, nil
+}
+
 func ParseInt(r *BencodeReader) (int, error) {
-	numberBuffer := make([]byte, 12)
-	bufferHead := 0
+	var numberBuffer []byte
+	//numberBuffer := make([]byte, 12)
+	//bufferHead := 0
+
 	confirmation, err := r.reader.ReadByte()
-	negativeFlag := -1
+	//negativeFlag := -1
+
 	if err != nil || confirmation != 'i' {
-		return 0, ParseIntError{}
+		return 0, ParseError{}
 	}
 
 	for {
 		b, err := r.reader.ReadByte()
 		if err != nil {
-			return 0, ParseIntError{err.Error()}
+			return 0, ParseError{err.Error()}
 		}
 
 		if b == 'e' {
 			break
 		}
 
-		if b == '-' {
-			if negativeFlag != -1 {
-				return 0, ParseIntError{"Duplicate hyphen"}
-			}
-
-			if bufferHead != 0 {
-				return 0, ParseIntError{"Misplaced hyphen"}
-			}
-
-			negativeFlag = 1
-			continue
-		}
-
 		if b < 48 || b > 57 {
-			return 0, ParseIntError{}
+			if b != '-' {
+				return 0, ParseError{"Illegal ascii code"}
+			}
 		}
 
-		if bufferHead < cap(numberBuffer) {
-			numberBuffer[bufferHead] = b
-		} else {
-			numberBuffer = append(numberBuffer, b)
-		}
-
-		bufferHead += 1
+		numberBuffer = append(numberBuffer, b)
 	}
 
-	if bufferHead == 0 {
-		return 0, ParseIntError{}
+	strRep := string(numberBuffer)
+
+	if strings.HasPrefix(strRep, "0") && len(strRep) > 1 {
+		return 0, ParseError{"Number can not start with 0"}
 	}
 
-	strRep := string(numberBuffer[:bufferHead])
-	if strings.HasPrefix(strRep, "0") && (len(strRep) > 1 || negativeFlag != -1) {
-		return 0, ParseIntError{}
+	if strings.HasPrefix(strRep, "-0") {
+		return 0, ParseError{"-0 is not permitted"}
 	}
 
-	result, err := strconv.Atoi(strRep[:bufferHead])
+	result, err := strconv.Atoi(strRep)
+
 	if err != nil {
-		return 0, ParseIntError{}
+		return 0, ParseError{err.Error()}
 	}
 
-	if negativeFlag == 1 {
-		return -result, nil
-	} else {
-		return result, nil
-	}
+	return result, nil
 }
 
 
--- bencode_test.go
+++ bencode_test.go
@@ -1,13 +1,14 @@
 package bencode
 
 import (
+	"fmt"
 	"io"
 	"strings"
 	"testing"
 )
 
-func TestBencode(t *testing.T) {
-	s := strings.NewReader("i-51e")
+func TestParseInt(t *testing.T) {
+	s := strings.NewReader("i-500000000000000e")
 	i := io.Reader(s)
 	bReader := NewBencodeReader(&i)
 
@@ -16,8 +17,31 @@ func TestBencode(t *testing.T) {
 		t.Error(err.Error())
 	}
 
-	if result != -51 {
+	if result != -500000000000000 {
 		t.Error("Error Parsing Int")
 	}
+}
 
+func TestParseIntNegative(t *testing.T) {
+	s := strings.NewReader("i--0e")
+	i := io.Reader(s)
+	bReader := NewBencodeReader(&i)
+
+	_, err := bReader.DecodeStream()
+	if err == nil {
+		t.Error("Double negation not being detected")
+	}
+}
+
+func TestParseBytes(t *testing.T) {
+	s := strings.NewReader("2:ab")
+	i := io.Reader(s)
+	bReader := NewBencodeReader(&i)
+
+	result, err := bReader.DecodeStream()
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	fmt.Printf("%q\n", result)
 }
